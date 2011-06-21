f594bc0affdaf3e03fe62d42ed6537aebf4d9bb4
Anton Podviaznikov <podviaznikov@gmail.com>
2011-06-21 14:55:27 +0000

    experiments

--- server.js
+++ server.js
@@ -3,7 +3,23 @@ var util = require('util'),
     connect = require('connect'),
     app = express.createServer();
 
-app.get('/',function(req,res){
+//configuration
+app.configure(function(){
+    app.use(connect.favicon(__dirname + '/public/16.png'));
+    //logger
+    app.use(express.logger());
+    //component for decoding requests' params
+    app.use(express.bodyParser());
+    //session support
+    app.use(express.cookieParser());
+    app.use(express.session({secret: 'super_hard_session_secret',cookie:{ path: '/', httpOnly: true, maxAge: 14400000000000000 }}));
+    //router
+    app.use(app.router);
+    //public folder for static files
+    app.use(express.static(__dirname+'/public'));
+});
+
+app.get('/test',function(req,res){
     res.write('Hello');
     res.end();
 });
